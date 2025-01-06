ad71e40403989866a6fda2a0e8f3a8b981c5dcfa
yu <yur.mpc@live.jp>
2025-01-06 20:25:41 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="">YOLO Object Detection in Browser</h1>
+      <p>Tensorflow.jsを使ってYOLOの物体検出を行うデモサイトです。</p>
+      <p>Detection, Segmantation, Oriented Bounding Boxに対応しています</p>
+    </>
+  )
+}
+
+export default App
