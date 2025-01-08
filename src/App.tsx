c6c601a54c5e1c6208509bba8cf10e40763ee897
yu <yur.mpc@live.jp>
2025-01-08 13:26:49 +0900

    lucide-reactを追加し、セグメンテーション表示機能を改善

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "@tensorflow/tfjs-backend-webgl";
 
 import * as tf from "@tensorflow/tfjs";
 
+import { LoaderCircle } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { convertImageElement, tensorFromPixel } from "./libs/conver_image_element";
 import { loadMetadata } from "./libs/load_metadata";
@@ -15,89 +16,138 @@ import { orientedView } from "./libs/view/oriented";
 import { segmentView } from "./libs/view/seg";
 
 function App() {
-  const [selectedModelType, setsSelectedModelType] = useState<ModelTaskType>(ModelTaskType.DETECT)
-  const [model, setModel] = useState<tf.GraphModel<string | tf.io.IOHandler> | null>(null)
-  const [metadata, setMetadata] = useState<YOLOMetadata | null>(null)
+  const [selectedModelType, setsSelectedModelType] = useState<ModelTaskType>(ModelTaskType.DETECT);
+  const [model, setModel] = useState<tf.GraphModel<string | tf.io.IOHandler> | null>(null);
+  const [metadata, setMetadata] = useState<YOLOMetadata | null>(null);
 
-  const [state, setState] = useState<[boolean, string]>([false, ""])
-  const imageRef = useRef<HTMLImageElement>(null)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [state, setState] = useState<[boolean, string]>([false, ""]);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const [imageSrc, setImageSrc] = useState<string>("./images/detect.jpg");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const maskCanvasRef = useRef<HTMLCanvasElement>(null);
 
   function handleChangeModel(event: React.ChangeEvent<HTMLSelectElement>) {
-    const taskType = event.target.value as ModelTaskType
-    setsSelectedModelType(taskType)
-    imageRef.current!.src = getImagePath(taskType)
+    const taskType = event.target.value as ModelTaskType;
+    setsSelectedModelType(taskType);
+    imageRef.current!.src = getImagePath(taskType);
   }
 
-  function detect(){
-    setState([true, "推論を実行しています"])
-    // 画像のTensorを取得
-    const convertedCanvas = convertImageElement(
-      imageRef.current!,
-      metadata!.imgsz
-    );
-    const imageTensor = tensorFromPixel(convertedCanvas, metadata!.imgsz);
-    const bboxes = predict(selectedModelType, model!, imageTensor, Object.entries(metadata!.names).length, 0.5, undefined)
-    const restoreScale = Math.max(
-      imageRef.current!.width / metadata!.imgsz[0],
-      imageRef.current!.height / metadata!.imgsz[1]
-    );
-    switch(selectedModelType){
-      case ModelTaskType.DETECT:
-        detectView(canvasRef.current!, imageRef.current!, restoreScale, bboxes as DetectBbox[])
-        break;
-      case ModelTaskType.ORIENTED:
-        orientedView(canvasRef.current!, imageRef.current!, restoreScale, bboxes as OrientedBbox[])
-        break
-      case ModelTaskType.SEGMENT:
-        segmentView(canvasRef.current!, imageRef.current!, restoreScale, bboxes as SegBbox[])
-        break
+  async function detect() {
+    setState([true, "推論を実行しています"]);
+    await new Promise((resolve) => setTimeout(resolve, 10));
+
+    await new Promise((resolve) => {
+      // 画像のTensorを取得
+      const convertedCanvas = convertImageElement(imageRef.current!, metadata!.imgsz);
+      const imageTensor = tensorFromPixel(convertedCanvas, metadata!.imgsz);
+      const bboxes = predict(selectedModelType, model!, imageTensor, Object.entries(metadata!.names).length, 0.4, undefined);
+      const restoreScale = Math.max(imageRef.current!.width / metadata!.imgsz[0], imageRef.current!.height / metadata!.imgsz[1]);
+      switch (selectedModelType) {
+        case ModelTaskType.DETECT:
+          detectView(canvasRef.current!, imageRef.current!, restoreScale, bboxes as DetectBbox[]);
+          break;
+        case ModelTaskType.ORIENTED:
+          orientedView(canvasRef.current!, imageRef.current!, restoreScale, bboxes as OrientedBbox[]);
+          break;
+        case ModelTaskType.SEGMENT:
+          segmentView(canvasRef.current!, maskCanvasRef.current!, imageRef.current!, metadata!.imgsz, bboxes as SegBbox[]);
+          break;
+      }
+      resolve(true);
+    });
+
+    setState([false, "完了"]);
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageSrc(reader.result as string); // 画像を表示
+      };
+      reader.readAsDataURL(file);
     }
-    setState([false, "完了"])
-
-  }
-
+  };
   useEffect(() => {
     const fetchData = async () => {
-      const path = getModelPath(selectedModelType)
+      const path = getModelPath(selectedModelType);
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
       // メタデータの読み込み
-      setState([true, "メタデータを読み込んでいます"])
+      setState([true, "メタデータを読み込んでいます"]);
       const metadata = await loadMetadata(path);
-      if(metadata === null){
-        setState([false, "メタデータの読み込みに失敗しました"])
-        return
+      if (metadata === null) {
+        setState([false, "メタデータの読み込みに失敗しました"]);
+        return;
       }
-      setMetadata(metadata)
-      
+      setMetadata(metadata);
+
       // modelの読み込み
-      setState([true, "モデルを読み込んでいます"])
-      const model = await loadYOLOModel(path, metadata.imgsz)
-      if(model === null){        
-        setState([false, "モデルの読み込みに失敗しました"])
-        return
+      setState([true, "モデルを読み込んでいます"]);
+      const model = await loadYOLOModel(path, metadata.imgsz);
+      if (model === null) {
+        setState([false, "モデルの読み込みに失敗しました"]);
+        return;
       }
-      setModel(model)
-      setState([false, "モデルの読み込みが完了しました"])
-    }
-    fetchData()
-  }, [selectedModelType])
+      setModel(model);
+      setState([false, "モデルの読み込みが完了しました"]);
+    };
+    fetchData();
+  }, [selectedModelType]);
 
   return (
-    <>
-      <h1 className="">Object Detection in Browser</h1>
-      <p>Tensorflow.jsとYOLOのモデルを使用して物体検出を行うデモサイトです。</p>
-      <p>Detection, Segmantation, Oriented Bounding Boxに対応しています</p>
-      <select onChange={handleChangeModel}>
-        <option value={ModelTaskType.DETECT}>Detection</option>
-        <option value={ModelTaskType.ORIENTED}>Oriented Bounding Box</option>
-        <option value={ModelTaskType.SEGMENT}>Segmentation</option>
-      </select>
-      <p>state: {state[1]}</p>
-      <button onClick={detect}>detect</button>
-      <img ref={imageRef} src="./images/seg.jpg" alt="sample" />
-      <canvas ref={canvasRef}></canvas>
-    </>
-  )
+    <div className=" flex flex-col gap-4 items-center py-12">
+      <h1 className=" text-3xl sm:text-4xl md:text-5xl text-center py-12 text-purple-800">Object Detection in Browser</h1>
+      <p className="text-center opacity-80">
+        Tensorflow.jsとYOLOのモデルを使用して物体検出を行うデモサイトです。
+        <br />
+        Detection, Oriented Bounding Box, Segmantationに対応しています
+      </p>
+
+      <div>
+        <label htmlFor="detect_type" className="block mb-2 text-sm font-medium text-center">
+          Detect Type
+        </label>
+        <select id="detect_type" onChange={handleChangeModel} className=" px-4 py-2 border-2 border-gray-400 rounded-xl ">
+          <option value={ModelTaskType.DETECT}>Detection</option>
+          <option value={ModelTaskType.ORIENTED}>Oriented Bounding Box</option>
+          <option value={ModelTaskType.SEGMENT}>Segmentation</option>
+        </select>
+      </div>
+      <label htmlFor="input_file" className="block mb-2 text-sm font-medium text-center">
+        Input file
+      </label>
+      <input id="input_file" ref={inputRef} type="file" accept="image/*" onChange={handleFileChange} className="mb-4" />
+      <div>
+        <label htmlFor="state" className="block mb-2 text-sm font-medium text-center">
+          state
+        </label>
+        <p className=" text-sm opacity-80">{state[1]}</p>
+      </div>
+      <button
+        onClick={() => detect()}
+        disabled={state[0]}
+        className={` inline-flex justify-center items-center px-8 py-2 rounded-lg h-12 w-32 text-center text-white text-xl ${state[0] ? "bg-cyan-500 opacity-50 " : "bg-cyan-500"}`}
+      >
+        {state[0] ? <LoaderCircle className="  h-6 w-6 animate-spin" /> : "検出"}
+      </button>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <h2 className="text-lg font-semibold mb-2 text-center">Input Image</h2>
+          <img ref={imageRef} src={imageSrc} alt="Sample for object detection" className="w-full max-w-[400px] h-auto rounded-lg shadow-md" />
+        </div>
+        <div>
+          <h2 className="text-lg font-semibold mb-2 text-center ">Detection Result</h2>
+          <canvas ref={canvasRef} className="w-full max-w-[400px] h-auto rounded-lg shadow-md"></canvas>
+        </div>
+      </div>
+      <canvas ref={maskCanvasRef} className="w-full max-w-[400px] h-auto rounded-lg shadow-md hidden"></canvas>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/libs/view/seg.ts
+++ src/libs/view/seg.ts
@@ -1,27 +1,100 @@
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+
 import { SegBbox } from "../types";
 
+const colors: number[][] = [
+  [204, 153, 255], // 紫系
+  [255, 153, 204], // ピンク系
+  [153, 255, 238], // エメラルドグリーン系
+  [255, 221, 153], // オレンジ系
+  [153, 170, 255], // 青紫系
+  [255, 153, 153], // 赤系
+  [153, 221, 255], // 水色系
+  [255, 170, 153], // コーラル系
+  [153, 255, 187], // ライトグリーン系
+  [221, 255, 153], // 黄緑系
+];
 
-export function segmentView(canvas: HTMLCanvasElement, image: HTMLImageElement, restoreScale: number, bboxes: SegBbox[]){
-    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-    
-    canvas.width = image.width
-    canvas.height = image.height
+export function segmentView(imageCanvas: HTMLCanvasElement, maskCanvas: HTMLCanvasElement, image: HTMLImageElement, imgsz: [number, number], bboxes: SegBbox[]) {
+  const processCtx = maskCanvas.getContext("2d", {
+    willReadFrequently: true,
+  }) as CanvasRenderingContext2D;
 
-    context.drawImage(image, 0, 0, image.width, image.height)
+  maskCanvas.width = 160;
+  maskCanvas.height = 160;
 
-    context.font = "10px Arial";
+  processCtx.fillStyle = "black"; // 塗りつぶす色を黒に設定
+  processCtx.fillRect(0, 0, maskCanvas.width, maskCanvas.height);
 
-    bboxes.forEach((bbox) => {
-      context.beginPath();
-      context.rect(
-        bbox.x * restoreScale,
-        bbox.y * restoreScale,
-        bbox.w * restoreScale,
-        bbox.h * restoreScale
-      );
-      context.strokeStyle = "salmon";
-      context.lineWidth = 4 * ((canvas.width ?? 640) / 640);
-      context.stroke();
+  const scale1 = 160 / imgsz[0]
+  bboxes.forEach((bbox, index) => {
+    bbox.mask.forEach((col, colIndex) => {
+      col.forEach((pixel, rowIndex) => {
+        if (pixel === 1) {
+          // bbox内にあるマスクのみ設定する
+          if(
+            (bbox.x * scale1 <= rowIndex && rowIndex <= (bbox.x + bbox.w) * scale1) &&
+            (bbox.y * scale1 <= colIndex && colIndex <= (bbox.y + bbox.h) * scale1)
+          ){
+            const imageData = processCtx.getImageData(rowIndex, colIndex, 1, 1);
+            imageData.data[0] = index + 1; // Rの数値にindex + 1を入れる
+            processCtx.putImageData(imageData, rowIndex, colIndex); // 変更したピクセルデータを反映
+          }
+        }
+      });
     });
+  });
 
+  const imageData = processCtx.getImageData(0, 0, maskCanvas.width, maskCanvas.height);
+
+  maskCanvas.width = image.naturalWidth;
+  maskCanvas.height = image.naturalHeight;
+  // 拡大して再描画
+  processCtx.imageSmoothingEnabled = true;
+  processCtx.putImageData(imageData, 0, 0);
+  const scale = Math.min(imgsz[0] / image.naturalWidth, imgsz[1] / image.naturalHeight);
+  const clip =
+    image.naturalWidth > image.naturalHeight
+      ? [imageData.width, imageData.height * ((image.naturalHeight * scale) / imgsz[1])]
+      : [imageData.width * ((image.naturalWidth * scale) / imgsz[0]), imageData.height];
+  processCtx.drawImage(maskCanvas, 0, 0, clip[0], clip[1], 0, 0, maskCanvas.width, maskCanvas.height);
+
+  // マスクデータ
+  const processData = processCtx.getImageData(0, 0, maskCanvas.width, maskCanvas.height);
+
+  // canvasに表示
+  const viewCtx = imageCanvas.getContext("2d", {
+    willReadFrequently: true,
+  }) as CanvasRenderingContext2D;
+
+  imageCanvas.width = image.naturalWidth;
+  imageCanvas.height = image.naturalHeight;
+
+  viewCtx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
+
+  const viewData = viewCtx.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+
+  const alphaBlend = 0.6;
+  for (let i = 0; i < processData.data.length; i += 4) {
+    const r = processData.data[i];
+    if (r !== 0) {
+      const color = colors[r % 10];
+      viewData.data[i] = Math.floor(viewData.data[i] * (1 - alphaBlend) + color[0] * alphaBlend); // R
+      viewData.data[i + 1] = Math.floor(viewData.data[i + 1] * (1 - alphaBlend) + color[1] * alphaBlend); // G
+      viewData.data[i + 2] = Math.floor(viewData.data[i + 2] * (1 - alphaBlend) + color[2] * alphaBlend); // B
+      viewData.data[i + 3] = 255; // 不透明のまま
+    }
+  }
+  viewCtx.putImageData(viewData, 0, 0);
+
+  const restoreScale = (1 / scale)
+  bboxes.forEach((bbox, index) => {
+    const color = colors[(index + 1) % 10]
+    viewCtx.beginPath();
+    viewCtx.rect(bbox.x * restoreScale, bbox.y * restoreScale, bbox.w * restoreScale, bbox.h * restoreScale);
+    viewCtx.strokeStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+    viewCtx.lineWidth = 5 * ((imageCanvas.width ?? 640) / 640);
+    viewCtx.stroke();
+  });
 }
